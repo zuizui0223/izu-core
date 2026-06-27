a9a1814e899f952e7d88f91e544d06a9ac811a9a
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-06-27 20:49:20 +0900

    Expose guide evolution extensions

--- channel_id/__init__.py
+++ channel_id/__init__.py
@@ -1,10 +1,12 @@
 """Tools for declared Campanula channel-identification studies."""
 
-from .discrimination import (
-    MeasurementOption,
-    MeasurementRanking,
-    rank_measurements,
-)
+from .discrimination import MeasurementOption, MeasurementRanking, rank_measurements
+from .guide_inbreeding import PostSeedResult, PostSeedSurvival, apply_post_seed_survival
+from .guide_multiguild import MultiguildGuideResult, PollinatorGuild, simulate_multiguild_guide_response
+from .guide_paternal import PaternalGuideParameters, PaternalGuideResult, simulate_guide_paternal_fitness
+from .guide_phenotype import FamilyGuideObservation, GuideReactionNorm
+from .guide_spatial import Patch, SpatialRecruitmentResult, distribute_seeds_to_patches
+from .guide_temporal import TemporalFitnessSummary, YearPerformance, summarise_temporal_fitness
 from .life_history import (
     CompatibilityResult,
     LifeHistoryParameters,
@@ -38,32 +40,14 @@ from .readiness import (
 )
 
 __all__ = [
-    "ChannelDesign",
-    "ChannelReadiness",
-    "ChannelReadinessReport",
-    "PollinatorComponentStatus",
-    "assess_channel_readiness",
-    "MeasurementOption",
-    "MeasurementRanking",
-    "rank_measurements",
-    "CompatibilityResult",
-    "LifeHistoryParameters",
-    "LifeHistoryResult",
-    "Metric",
-    "ObservationInterval",
-    "ParameterGrid",
-    "Regime",
-    "SimulationCase",
-    "TraitState",
-    "assess_compatibility",
-    "retain_compatible_candidates",
-    "simulate_life_history",
-    "GuideContrast",
-    "GuideSelectionDirection",
-    "NectarGuideParameters",
-    "NectarGuideRegime",
-    "NectarGuideResult",
-    "NectarGuideTrait",
-    "compare_guide_phenotypes",
-    "simulate_nectar_guide_life_history",
+    "ChannelDesign", "ChannelReadiness", "ChannelReadinessReport", "PollinatorComponentStatus", "assess_channel_readiness",
+    "MeasurementOption", "MeasurementRanking", "rank_measurements",
+    "CompatibilityResult", "LifeHistoryParameters", "LifeHistoryResult", "Metric", "ObservationInterval", "ParameterGrid", "Regime", "SimulationCase", "TraitState", "assess_compatibility", "retain_compatible_candidates", "simulate_life_history",
+    "GuideContrast", "GuideSelectionDirection", "NectarGuideParameters", "NectarGuideRegime", "NectarGuideResult", "NectarGuideTrait", "compare_guide_phenotypes", "simulate_nectar_guide_life_history",
+    "PaternalGuideParameters", "PaternalGuideResult", "simulate_guide_paternal_fitness",
+    "PollinatorGuild", "MultiguildGuideResult", "simulate_multiguild_guide_response",
+    "PostSeedSurvival", "PostSeedResult", "apply_post_seed_survival",
+    "YearPerformance", "TemporalFitnessSummary", "summarise_temporal_fitness",
+    "GuideReactionNorm", "FamilyGuideObservation",
+    "Patch", "SpatialRecruitmentResult", "distribute_seeds_to_patches",
 ]
